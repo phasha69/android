7fbb3597f12b177005d8c71e13277ce2d82129c9
Pavel Bodrug <plkoij73@gmail.com>
2022-06-19 00:57:49 +0300

    invisible navigation bar

--- Sculptor/app/src/main/java/first/bodrug/sculptor/MainActivity.kt
+++ Sculptor/app/src/main/java/first/bodrug/sculptor/MainActivity.kt
@@ -2,10 +2,16 @@ package first.bodrug.sculptor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION +
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+       // actionBar?.hide()
         setContentView(R.layout.activity_main)
     }
 }

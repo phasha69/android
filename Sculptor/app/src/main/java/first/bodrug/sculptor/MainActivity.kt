c8537f754c2a5f5fffe1cb9e56d3fd344bcf9862
Pavel Bodrug <plkoij73@gmail.com>
2022-06-27 01:30:13 +0300

    strings recourse

--- Sculptor/app/src/main/java/first/bodrug/sculptor/MainActivity.kt
+++ Sculptor/app/src/main/java/first/bodrug/sculptor/MainActivity.kt
@@ -3,15 +3,16 @@ package first.bodrug.sculptor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.Window
+import androidx.appcompat.app.AppCompatDelegate
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION +
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-
-       // actionBar?.hide()
+        actionBar?.hide()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
     }
 }

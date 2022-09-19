3903909f7bc9554129ba4259b741ff3df51828b9
Pavel Bodrug <plkoij73@gmail.com>
2022-09-19 22:14:15 +0300

    Done

--- m1_hello_world/app/src/main/java/first/bodrug/m1_hello_world/MainActivity.kt
+++ m1_hello_world/app/src/main/java/first/bodrug/m1_hello_world/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import first.bodrug.m1_hello_world.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val bindings = ActivityMainBinding.inflate(layoutInflater)
@@ -14,44 +15,42 @@ class MainActivity : AppCompatActivity() {
 
         bindings.textInfo.text = START_MESSAGE
         bindings.textInfo.setTextColor(Color.GREEN)
-        bindings.buttonMinus.isEnabled= false
-        bindings.buttonMinus.setOnClickListener {
-            if (count > ZERO) {
-                count--
-                bindings.counter.text = "$count"
-                if (count <= FULL) {
+        bindings.buttonMinus.isEnabled = false
+
+        fun setBussInfo(count: Int) {
+            bindings.counter.text = "$count"
+            when (count) {
+                ZERO -> {
+                    bindings.buttonMinus.isEnabled = false
+                    bindings.textInfo.setTextColor(Color.GREEN)
+                    bindings.textInfo.text = START_MESSAGE
+                    bindings.reset.visibility = View.INVISIBLE
+                }
+                in (1..FULL) -> {
+                    bindings.buttonMinus.isEnabled = true
                     bindings.textInfo.setTextColor(Color.BLUE)
                     bindings.textInfo.text = free
                     bindings.reset.visibility = View.INVISIBLE
                 }
-                if (count == ZERO) {
-                    bindings.textInfo.text = START_MESSAGE
-                    bindings.textInfo.setTextColor(Color.GREEN)
-                    bindings.buttonMinus.isEnabled = false
+                else -> {
+                    bindings.reset.visibility = View.VISIBLE
+                    bindings.textInfo.setTextColor(Color.RED)
+                    bindings.textInfo.text = END_MESSAGE
                 }
             }
         }
 
+        bindings.buttonMinus.setOnClickListener {
+            setBussInfo(--count)
+        }
+
         bindings.buttonPlus.setOnClickListener {
-            count++
-            bindings.buttonMinus.isEnabled = true
-            bindings.counter.text = "$count"
-            bindings.textInfo.text = free
-            bindings.textInfo.setTextColor(Color.BLUE)
-            if (count > FULL) {
-                bindings.reset.visibility = View.VISIBLE
-                bindings.textInfo.setTextColor(Color.RED)
-                bindings.textInfo.text = END_MESSAGE
-            }
+            setBussInfo(++count)
         }
 
         bindings.reset.setOnClickListener {
             count = ZERO
-            bindings.buttonMinus.isEnabled = false
-            bindings.textInfo.setTextColor(Color.GREEN)
-            bindings.counter.text = "$count"
-            bindings.textInfo.text = START_MESSAGE
-            bindings.reset.visibility = View.INVISIBLE
+            setBussInfo(count)
         }
     }
 
@@ -59,7 +58,7 @@ class MainActivity : AppCompatActivity() {
         const val ZERO = 0
         var count = ZERO
         val free: String
-            get() = "Мест осалось ${FULL - count}"
+            get() = "Мест осталось ${FULL - count}"
         const val FULL = 50
         const val START_MESSAGE = "Все места свободны"
         const val END_MESSAGE = "Пассажиров слишком много!"
